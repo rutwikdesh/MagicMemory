de3b438dc072574126d051ba4345a1a5f8bf21c9
Rutwik Deshmukh <rutwikdesh28@gmail.com>
2023-01-24 13:32:33 +0530

    added cards and matched prop

--- src/App.js
+++ src/App.js
@@ -1,10 +1,74 @@
+import { useEffect, useState } from 'react';
+import SingleCard from './components/SingleCard';
 import './App.css';
 
+const cardImages = [
+  { "src": "/img/helmet.png" },
+  { "src": "/img/potion.png" },
+  { "src": "/img/ring.png" },
+  { "src": "/img/scroll.png" },
+  { "src": "/img/shield.png" },
+  { "src": "/img/sword.png" }
+]
+
 function App() {
+  const [cards, setCards] = useState([])
+  const [turns, setTurns] = useState(0)
+  const [choiceOne, setChoiceOne] = useState(null)
+  const [choiceTwo, setChoiceTwo] = useState(null)
+
+  //shuffle cards
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }))
+
+    setCards(shuffledCards)
+    setTurns(0)
+  }
+
+  const handleChoice = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
+  }
+
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      if (choiceOne.src === choiceTwo.src) {
+        setCards(prevCards => {
+          return prevCards.map(card => {
+            if (card.src == choiceOne) {
+              return { ...card, matched: true }
+            }
+            else {
+              return card
+            }
+          })
+        })
+        resetTurn()
+      }
+      else {
+
+        resetTurn()
+      }
+    }
+  }, [choiceOne, choiceTwo])
+
+  const resetTurn = () => {
+    setChoiceOne(null)
+    setChoiceTwo(null)
+    setTurns(turns + 1)
+  }
+
   return (
     <div className="App">
       <h1>Match the Card</h1>
-      <button>New Game</button>
+      <button onClick={shuffleCards}>New Game</button>
+
+      <div className="card-grid">
+        {cards.map(card => (
+          <SingleCard key={card.key} card={card} handleChoice={handleChoice} />
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/components/SingleCard.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './SingleCard.css'
+
+export default function SingleCard({ card, handleChoice }) {
+
+  const handleClick = () => {
+    handleChoice(card)
+  }
+
+  return (
+    <div className="card">
+      <div>
+        <img className="front" src={card.src} alt="card front" hidden />
+        <img className="back" src="/img/cover.png" alt="card back" onClick={handleClick} />
+      </div>
+    </div>
+  )
+}
